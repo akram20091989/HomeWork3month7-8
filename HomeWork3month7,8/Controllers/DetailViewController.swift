55c44d748c4dc65aca582766c4732b75d370d26d
akram <akram20091989@gmail.com>
2023-06-12 11:44:59 +0600

    Initial Commit

---
+++ HomeWork3month7,8/Controllers/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  HomeWork3month7,8
+//
+//  Created by bond on 7/6/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    private let tapButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Tap", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 40)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .tintColor
+        button.layer.cornerRadius = 24
+        button.addTarget(self, action: #selector(tap), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func tap(){
+        let vc = NextViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(tapButton)
+        
+        tapButton.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+            make.height.width.equalTo(250)
+        }
+    }
+    
+
+}
---
+++ HomeWork3month7,8/Model/Contacts.swift
@@ -0,0 +1,13 @@
+//
+//  Contacts.swift
+//  HomeWork3month7,8
+//
+//  Created by bond on 8/6/23.
+//
+
+import Foundation
+
+struct Contact{
+    var name: String
+    var adress: String
+}
